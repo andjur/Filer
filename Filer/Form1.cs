19b0dd7e9d87fb66e662235c9bb6a8e89818e8ac
andrej.jursa@gmail.com <andrej.jursa@gmail.com>
2021-02-19 23:11:10 +0100

    scan made asynchronous to avoid UI lockup

--- Filer/Form1.cs
+++ Filer/Form1.cs
@@ -42,12 +42,24 @@ namespace Filer
             {
                 var files = Directory.GetFiles(directory);
 
-                rtbOutput.AppendText(directory + String.Format(" {0} file(s)", files.Count()) + "\n");
-                Application.DoEvents();
+                //rtbOutput.AppendText(directory + String.Format(" {0} file(s)", files.Count()) + "\n");
+                //Application.DoEvents();
 
                 foreach (string file in files)
                 {
                     var fi = new FileInfo(file);
+
+                    Object md5 = DBNull.Value;
+                    try
+                    {
+                        if (includeChecksum)
+                            md5 = CalculateMD5(file);
+                    }
+                    catch (IOException /*ioex*/)
+                    {
+                        // FIXME: report unable to compute MD5
+                    }
+
                     cmd = conn.CreateCommand();
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.CommandText = "[dbo].[File_insert]";
@@ -57,7 +69,7 @@ namespace Filer
                     cmd.Parameters.Add(new SqlParameter("@Size", fi.Length));
                     cmd.Parameters.Add(new SqlParameter("@Created", fi.CreationTime));
                     cmd.Parameters.Add(new SqlParameter("@Modified", fi.LastWriteTime));
-                    cmd.Parameters.Add(new SqlParameter("@MD5", includeChecksum ? CalculateMD5(file) : DBNull.Value));
+                    cmd.Parameters.Add(new SqlParameter("@MD5", md5));
                     cmd.ExecuteNonQuery();
                 }
 
@@ -69,7 +81,7 @@ namespace Filer
             }
             catch (Exception ex)
             {
-                rtbOutput.AppendText(ex.ToString() + "\n");
+                //rtbOutput.AppendText(ex.ToString() + "\n");
             }
         }
 
@@ -98,40 +110,49 @@ namespace Filer
             return null;
         }
 
-        private void btnScan_Click(object sender, EventArgs e)
+        private async void btnScan_Click(object sender, EventArgs e)
         {
+            txtFolder.Text = txtFolder.Text.Trim(); // normalize input
+
             var stopwatch = Stopwatch.StartNew();
-            {
-                txtFolder.Text = txtFolder.Text.Trim();
-                var startPath = txtFolder.Text;
-                var driveLabel = GetDriveLabel(startPath);
-                using (var conn = new SqlConnection(txtConnectionString.Text))
-                {
-                    conn.Open();
-                    var cmd = conn.CreateCommand();
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.CommandText = "[dbo].[Scan_insert]";
-                    cmd.Parameters.Add(new SqlParameter("@StartPath", startPath));
-                    cmd.Parameters.Add(new SqlParameter("@DriveLabel", driveLabel));
-                    //cmd.Parameters.Add(new SqlParameter("@DriveSerialNumber", N/A));
-                    SqlParameter parId = new SqlParameter("@Id", DBNull.Value);
-                    parId.SqlDbType = SqlDbType.BigInt;
-                    parId.Direction = ParameterDirection.Output;
-                    cmd.Parameters.Add(parId);
-                    cmd.ExecuteNonQuery();
-                    var scanId = (long)parId.Value;
 
-                    ProcessDirectory(conn, scanId, startPath, chkIncludeMd5.Checked);
+            await Task.Run(() => 
+            { 
+                Scan(txtFolder.Text, chkIncludeMd5.Checked, txtConnectionString.Text); 
+            });
 
-                    cmd = conn.CreateCommand();
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.CommandText = "[dbo].[Scan_update_Completed]";
-                    cmd.Parameters.Add(new SqlParameter("@Id", scanId));
-                    cmd.ExecuteNonQuery();
-                }
-            }
             stopwatch.Stop();
             rtbOutput.AppendText("Elapsed time: " + stopwatch.Elapsed.ToString(@"h\:mm\:ss\.fff") + " (" + (chkIncludeMd5.Checked ? "with" : "without") + " checksums)\n\n");
+        }
+
+        private void Scan(string folder, bool includeMd5, string connectionString)
+        {
+            var startPath = folder;
+            var driveLabel = GetDriveLabel(startPath);
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[dbo].[Scan_insert]";
+                cmd.Parameters.Add(new SqlParameter("@StartPath", startPath));
+                cmd.Parameters.Add(new SqlParameter("@DriveLabel", driveLabel));
+                //cmd.Parameters.Add(new SqlParameter("@DriveSerialNumber", N/A));
+                SqlParameter parId = new SqlParameter("@Id", DBNull.Value);
+                parId.SqlDbType = SqlDbType.BigInt;
+                parId.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(parId);
+                cmd.ExecuteNonQuery();
+                var scanId = (long)parId.Value;
+
+                ProcessDirectory(conn, scanId, startPath, includeMd5);
+
+                cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[dbo].[Scan_update_Completed]";
+                cmd.Parameters.Add(new SqlParameter("@Id", scanId));
+                cmd.ExecuteNonQuery();
+            }
         }
 
         /*
